9a079788ca9d3806f0357c42084e5ee86bf6a627
jreleaserbot <jreleaser@kordamp.org>
2023-07-13 09:16:06 +0000

    spring-cli v0.0.8

--- Formula/spring-cli.rb
+++ Formula/spring-cli.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.7.0 at 2023-07-11T12:53:19.118816145Z
+# Generated with JReleaser 1.7.0 at 2023-07-13T09:15:42.35136413Z
 class SpringCli < Formula
   desc "Spring Cli"
   homepage "https://github.com/rd-1-2022/spring-cli-jr"
-  url "https://github.com/rd-1-2022/spring-cli-jr/releases/download/v0.0.7/spring-cli-0.0.7.zip"
-  version "0.0.7"
-  sha256 "f0e38b9560d422164df1c7ed51f4e2e1a6237779652061b3d4d871ba71dc97e0"
+  url "https://github.com/rd-1-2022/spring-cli-jr/releases/download/v0.0.8/spring-cli-0.0.8.zip"
+  version "0.0.8"
+  sha256 "7d7cc07bd4b42bcd4694d8f24771311bdf149f2c5b88788967a1b018d206e510"
   license "Apache-2.0"
 
   depends_on "openjdk@17"
@@ -16,6 +16,6 @@ class SpringCli < Formula
 
   test do
     output = shell_output("#{bin}/spring --version")
-    assert_match "0.0.7", output
+    assert_match "0.0.8", output
   end
 end
